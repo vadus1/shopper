1e08dadea71b8fd1da3555ab51f63e13001c7561
vadus1 <bekish94.94@mail.ru>
2014-12-19 18:24:14 +0300

    Fix icon

--- config/application.rb
+++ config/application.rb
@@ -50,7 +50,6 @@ module Shopper
     config.assets.enabled = true
 
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
-    config.assets.precompile += %w(favicon.ico)
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
