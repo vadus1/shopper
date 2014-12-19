f963fc9d1a7368a41f6d60263452a4144a2b7b85
vadus1 <bekish94.94@mail.ru>
2014-12-19 18:20:04 +0300

    compile favicon and fix title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -65,4 +65,13 @@ module ApplicationHelper
       end
     end
   end
+
+  def full_title page_title
+    base_title = "Tokebi"
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
 end
